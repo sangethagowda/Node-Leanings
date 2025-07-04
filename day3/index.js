b9d49ef659fea7b215dd6f95ddced3a230175b77
Sangeetha <sangeethaGowda@gmail.com>
2025-07-05 00:27:54 +0530

    new update

--- day3/index.js
+++ day3/index.js
@@ -2,24 +2,29 @@ const http = require("http");
 const fs = require("fs");
 
 // just creates server
-const myServer = http.createServer((req,res)=>{
-    const log = `New request at time :${Date.now()}`
-    fs.appendFile("log.txt",log,(err,data)=>{
-        switch(res.url){
-            case "/":res.end("Home")
-            break;
-            case "/comtact":res.end("contact 918265365")
-            break;
-        }
-    })
+// const myServer = http.createServer((req,res)=>{
+//     const log = `New request at time :${Date.now()}`
+//     fs.appendFile("log.txt",log,(err,data)=>{
+//         switch(res.url){
+//             case "/":res.end("Home")
+//             break;
+//             case "/comtact":res.end("contact 918265365")
+//             break;
+//         }
+//     })
 
+// });
+
+// myServer.listen(8000)
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
 });
-
-myServer.listen(8000)
-
-
-const server2=http.createServer((req,res)=>{
-    res.end("response from server 2")
-})
-
-server2.listen(8001)
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
